ed2fb5068fb9036cf7f257692f62d0684c3c2065
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-02-18 11:19:09 -0300

    feat: create UserList

--- src/models/UserList.js
+++ src/models/UserList.js
@@ -10,4 +10,27 @@ class UserList {
     getAllUsers(){
         return this.users;
     }
+
+    getUserById(id){
+        const user = this.users.find(user => user.id == id);
+        if(!user){
+            throw new Error("Usuário não encontrado");
+        }
+        return user;
+    }
+
+    updateUser(id, updateData) {
+        const user = getUserById(id);
+        Object.assign(user, updateData);
+        return user;
+    }
+    deleteUser(id) {
+        this.users = this.users.filter(user => user.id != id);
+    }
+
+    getTotalUsers(){
+        return this.users.length;
+    }
 }
+
+module.exports = UserList;
