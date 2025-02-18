988f01bf8377c8fbf771042674c2712e134ff313
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-02-18 11:30:02 -0300

    Feat: create addUser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const userRoutes = require("./src/routes/usersRoutes");
 
 const app = express();
 const PORT = 4000;
@@ -7,6 +8,7 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/api", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Euuu amooooo backend 🚀🚀 ");
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -5,8 +5,25 @@ const Lista = new UserList();
 
 Lista.addUSer(new User("Leonardo", "leonardo@gmail.com", 17));
 Lista.addUSer(new User("Luiz", "luiz@gmail.com", 16));
+Lista.addUSer(new User("Thiago", "Thiago@gmail.com", 25));
+Lista.addUSer(new User("Marcello", "marcello@gmail.com", 35));
+
 
 const router = {
+    addUser: (req, res) => {
+        try {
+            const { name, email, idade} =  req.body;
+            if(!name || !email || !idade) {
+                throw new Error('Preencha todos os campos pedidos!!');
+            }
+            const usuario = new User (name, email, idade);
+            lista.addUser(usuario);
+            res.status(200).json({message: "Usuário criado com sucesso"});
+        } catch (error) {
+            res.status(400).json({message: "Erro ao criar ao criar o usuário", error});
+        }
+    },
+    
     getAllUsers: (req, res) =>{
         res.json(Lista.getAllUsers());
     },
